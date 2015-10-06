d11784541f00fc2ec0cfad8bb3ee99a1b4060240
Naaz <marziyeh.naaz.ahmed@gmail.com>
2015-10-06 09:14:10 +0100

    all

--- main.js
+++ main.js
@@ -1,27 +1,20 @@
 
 
-document.getElementById('Contact').addEventListener('click', contactClick);
+document.getElementById('contact').addEventListener('click', contactClick);
 
 function contactClick() {
-  // var splashDiv = document.getElementById('splash');
-  // var wrapper = document.getElementsByClassName('wrapper')[0];
-  // var body = document.getElementsByTagName('body')[0];
-  var coverHeading = document.getElementsByClassName('cover-heading')[0];
+  var form = document.getElementById('mc_embed_signup');
+  var intro = document.getElementsByClassName('intro')[0];
+  intro.style.display = "none";
+  form.style.display="block";
+}
 
+document.getElementById('home').addEventListener('click', homeClick);
 
-  coverHeading.style.display = "none";
-//   splashDiv.style.opacity=0;
-// splashDiv.style.animate=
-  // // splashDiv.style.transform = "translate3d(33.3333%, 0px, 0px)";
-  // splashDiv.style.transform = "scale3d(1, 1, 1)";
-  // google.style.display = "block";
-
-
-//shows and styles the education/github section
-  // wrapper.style.display = "block";
-  // wrapper.style["margin-top"] = 0;
-  // body.style["overflow-y"] = "auto";
-  // body.style.display = "block";
-  // box.style["background-color"] = "rgb(136, 132, 119)";
+function homeClick() {
+  var form = document.getElementById('mc_embed_signup');
+  var intro = document.getElementsByClassName('intro')[0];
+  form.style.display="none";
+  intro.style.display = "block";
 
 }
